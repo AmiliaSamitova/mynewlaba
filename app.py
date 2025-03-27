2f9e8e5d0c7fa3abdf7815356f0061a2c687b08f
AmiliaSamitova <159034975+AmiliaSamitova@users.noreply.github.com>
2025-03-27 20:46:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 titanic = pd.read_csv("titanic.csv")
 
@@ -9,3 +11,13 @@ st.write(titanic.describe())
 st.write('Столбцы и их типы:')
 st.write(titanic.dtypes)
 st.write('Форма таблицы:', titanic.shape)
+
+#пользователь может выставить определнное количество строк таблицы, по умолчанию 5 
+num_rows = st.number_input('Количество строк:', min_value=1, max_value=len(titanic), value=5)
+st.write(titanic.head(num_rows))
+
+st.subheader('Графики')
+#гистограмма распределения возрастов
+st.subheader('Распределение возрастов пассажиров')
+sns.histplot(titanic['Age'].dropna(), bins=30) #dropna() удаляет пропуски
+st.pyplot()
