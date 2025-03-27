336dcc5af416809171e6a97f85bc789f18bc055f
AmiliaSamitova <159034975+AmiliaSamitova@users.noreply.github.com>
2025-03-27 20:32:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-titanic = pd.read_csv("")
+titanic = pd.read_csv("titanic.csv")
+
+st.title('Данные пассажиров Титаника')
+st.subheader('Описательная статистика')
+st.write(titanic.describe())
+st.write('Столбцы и их типы:')
+st.write(titanic.dtypes)
+st.write('Форма таблицы:', titanic.shape)
