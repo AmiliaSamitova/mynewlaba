c2b418b1a4210001a2092505f0cd0a4e73064afe
AmiliaSamitova <159034975+AmiliaSamitova@users.noreply.github.com>
2025-03-28 00:17:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,67 @@ st.write('Столбцы и их типы:')
 st.write(titanic.dtypes)
 st.write('Форма таблицы:', titanic.shape)
 
-#пользователь может выставить определнное количество строк таблицы, по умолчанию 5 
+#пользователь может выставить определенное количество строк таблицы, по умолчанию 5 
 num_rows = st.number_input('Количество строк:', min_value=1, max_value=len(titanic), value=5)
 st.write(titanic.head(num_rows))
 
 st.subheader('Графики')
-#гистограмма распределения возрастов
-st.subheader('Распределение возрастов пассажиров')
-fig, ax = plt.subplots()
-sns.histplot(titanic['Age'].dropna(), bins=30, ax=ax)
-st.pyplot(fig)
+#строим гистограммы в зависимости от параметра,который выбирает пользователь
+choice = st.selectbox('Выберите класс:',['pclass', 'sex', 'Age', 'embarked'] )
+st.subheader(f'Распределение людей на Титанике по параметру {choice}')
+if choice == 'pclass':
+    plt.figure(figsize=(10, 5))
+    sns.histplot(titanic['Pclass'].dropna(), bins=30) 
+    st.pyplot(plt.gcf())
+elif choice == 'sex':
+    plt.figure(figsize=(10, 5))
+    sns.histplot(titanic['Sex'].dropna(), bins=30) 
+    st.pyplot(plt.gcf())
+elif choice == 'Age':
+    plt.figure(figsize=(10, 5))
+    sns.histplot(titanic['Age'].dropna(), bins=30) 
+    st.pyplot(plt.gcf())
+else:
+    plt.figure(figsize=(10, 5))
+    sns.histplot(titanic['Embarked'].dropna(), bins=30) 
+    plt.legend(title="Порты", labels=['Southampton, Cherbourg, Queenstown' ])
+    st.pyplot(plt.gcf())
+
+
+
+#график выживаемости по полу
+plt.figure(figsize=(10, 5))
+st.subheader('Выживаемость в зависимости от пола')
+sns.countplot(x='Sex', hue='Survived', data=titanic) #hue позволяет отобразить данные разных категорий в легенде
+st.pyplot(plt.gcf()) #возвращает текущую фигуру, созданную с помощью библиотеки Matplotlib
+
+#график выживаемости по возрасту
+plt.figure(figsize=(40, 20))
+st.subheader('Выживаемость в зависимости от возраста')
+sns.countplot(x='Age', hue='Survived', data=titanic) 
+st.pyplot(plt.gcf())
+
+#график выживаемости по классу
+plt.figure(figsize=(10, 5))
+st.subheader('Выживаемость в зависимости от класса')
+sns.countplot(x='Pclass', hue='Survived', data=titanic) 
+st.pyplot(plt.gcf())
+
+#диаграмма количеств различных классов
+plt.figure(figsize=(10, 5))
+st.subheader('Распределение по Ticket class')
+class_counts = titanic['Pclass'].value_counts()
+plt.pie(class_counts, labels=class_counts.index)
+plt.axis('equal')  # Сделать круговой график кругом
+st.pyplot(plt.gcf())
+
+#график, отображающий среднюю стоимость билетов по классам
+plt.figure(figsize=(8, 6))
+average_fare = titanic.groupby('Pclass')['Fare'].mean()
+average_fare.plot(kind='line')
+plt.title('Средняя стоимость билета по классам на Титанике')
+plt.xlabel('Класс')
+plt.ylabel('Средняя стоимость')
+
+plt.tight_layout()
+st.pyplot(plt.gcf())
